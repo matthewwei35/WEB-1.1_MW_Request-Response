fe9f00664b2d4124aa8431bb19572f88ea4e6e3c
Matthew Wei <matthewwei35@gmail.com>
2021-01-26 15:58:19 -0800

    Completed Your User’s Favorite Animal

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def homepage():
   """Shows a greeting to the user."""
   return 'Are you there, world? It\'s me, Ducky!'
 
+@app.route('/animal/<users_animal>')
+def favorite_animal(users_animal):
+  """Display a message to the user that changes based on their favorite animal."""
+  return f'Wow, {users_animal} is my favorite animal, too!'
+
 if __name__ == '__main__':
   app.run(debug=True)
