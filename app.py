94fbffc3bdb231a28db5ffd3a8d4ccf6d2096cac
Matthew Wei <matthewwei35@gmail.com>
2021-01-26 15:53:34 -0800

    Completed Hello, World!

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-
 # TODO: Follow the assignment instructions to complete the required routes!
 # (And make sure to delete this TODO message when you're done!)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+  """Shows a greeting to the user."""
+  return 'Are you there, world? It\'s me, Ducky!'
+
+if __name__ == '__main__':
+  app.run(debug=True)
