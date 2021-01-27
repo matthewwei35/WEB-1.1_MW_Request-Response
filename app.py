a461cfdfce582612dd0453ebddfed9205d79b1b7
Matthew Wei <matthewwei35@gmail.com>
2021-01-26 16:01:43 -0800

    Completed Your User’s Favorite Dessert

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def favorite_animal(users_animal):
   """Display a message to the user that changes based on their favorite animal."""
   return f'Wow, {users_animal} is my favorite animal, too!'
 
+@app.route('/dessert/<users_dessert>')
+def favorite_dessert(users_dessert):
+  return f'How did you know I liked {users_dessert}?'
+
 if __name__ == '__main__':
   app.run(debug=True)
